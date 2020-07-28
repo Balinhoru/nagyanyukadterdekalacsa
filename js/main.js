8bf2361a396640703f24916ac1364925d23566a4
balinhoru <balinhoru@gmail.com>
2020-07-28 13:46:13 +0200

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ function getServerData(url) {
         response => response.json(),
         err => console.error(err)
     )
-    
+
 }
 
-document.querySelector("#getDataBtn").addEventListener("click", function() {
+document.querySelector("#getDataBtn").addEventListener("click", function () {
     getServerData("http://localhost:3000/users").then(
         data => fillDataTable(data, "userTable")
-    )  
+    )
 })
 
 // Fill table with server data.
 function fillDataTable(data, tableID) {
-    let table = document.querySelector(`#${talbeID}`)
+    let table = document.querySelector(`#${tableID}`)
     if (!table) {
         console.error(`Table "${tableID}" is not found.`)
         return
@@ -29,6 +29,35 @@ function fillDataTable(data, tableID) {
 
     let tbody = table.querySelector("tbody")
     for (let row of data) {
-        console.log(row)
+        let tr = createAnyElement("tr")
+        for (let k in row) {
+            let td = createAnyElement("td")
+            td.innerHTML = row[k]
+            tr.appendChild(td)
+        }
+        let btnGroup = createBtnGroup()
+        tr.appendChild(btnGroup)
+        tBody.appendChild(tr)
     }
+}
+
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name)
+    for (let k in attributes) {
+        element.setAttributes(k, attributes[k])
+    }
+    return element
+}
+function createBtnGroup() {
+    let group = createAnyElement("div", {class: "btn btn-group"})
+    let infoBtn = createAnyElement("button", {class: "btn btn-info"})
+    let delBtn = createAnyElement("button", {class: "btn btn-danger"})
+
+    group.appendChild(infoBtn)
+    group.appendChild(delBtn)
+
+    let td = createAnyElement("td")
+    td.appendChild(group)
+
+    return td
 }
